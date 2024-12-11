6f88d2dde642ebef4671971f497d9b4867ba24a7
Swetha <swetha.muralitharan@axians.co.uk>
2024-12-11 11:45:19 +0000

    completes exercises

--- cli/transaction.js
+++ cli/transaction.js
@@ -1,12 +1,25 @@
-import { Command } from 'commander'
+import { Command } from 'commander';
 
-const transactionController = new Command('transaction')
+const transactionController = new Command('transaction');
 
+function logTransaction(from, to, amount) {
+  const date = new Date();
+  const formattedDate = date.toLocaleString('en-GB', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: true
+  });
+
+  console.log(`At ${formattedDate}, ${from} sent ${to} £${amount}`);
+}
+
+// Register the log command
 transactionController
   .command('log <from> <to> <amount>')
   .description('Log transaction data to the console')
-  .action((from, to, amount) => {
-    // TODO
-  })
+  .action(logTransaction);
 
-export default transactionController
+export default transactionController;
