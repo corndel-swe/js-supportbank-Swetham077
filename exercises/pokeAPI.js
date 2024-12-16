aa07f8a2117d9a7e62c321ca9135cc23f7ad43a7
Swetha <swetha.muralitharan@axians.co.uk>
2024-12-16 11:02:32 +0000

    completes day 3 exercise 2

--- exercises/pokeAPI.js
+++ exercises/pokeAPI.js
@@ -1,3 +1,7 @@
 export async function getPokemonList() {
   // TODO
+const repsonse = await fetch('https://pokeapi.co/api/v2/pokemon');
+
+const data = await repsonse.json();
+return data.results.map(pokemon => pokemon.name);
 }
